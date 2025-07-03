cde9b82aec338e736de5aebc443a98e03feed4e5
MmaGod1 <gmmaduchukwu@gmail.com>
2025-07-03 22:47:14 +0100

    build a real-time character counter using JavaScript

---
+++ JavaScript/real_time_counter/script.js
@@ -0,0 +1,21 @@
+const textInput= document.getElementById("text-input");
+
+const countChar = document.getElementById("char-count")
+
+function countCharacters(input) {
+ let count = input.target.value.length;
+ 
+ console.log(count)
+ if (count > 50) {
+   input.target.value = input.target.value.slice(0, 50);
+   count = 50
+ }
+ countChar.innerHTML = `Character Count: ${count}/50`;
+ 
+ if (count => 50) {
+   countChar.style.color = "red";
+ }
+}
+
+textInput.addEventListener('input', countCharacters);
+
