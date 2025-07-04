50cfef18534f3ae1037e9c6a2fcb64f7607769de
MmaGod1 <gmmaduchukwu@gmail.com>
2025-07-04 16:45:32 +0100

    add a lightbox gallery viewer project

---
+++ JavaScript/lightbox_viewer/script.js
@@ -0,0 +1,27 @@
+const lightBox = document.querySelector(".lightbox");
+const closeBtn = document.getElementById("close-btn");
+const lightBoxImg = document.getElementById("lightbox-image");
+const thumbnails = document.querySelectorAll(".gallery-item");
+
+function popImage(image) {
+  const fullImgSrc = image.src.replace("-thumbnail", "");
+  lightBoxImg.src = fullImgSrc;
+  lightBox.style.display = "flex";
+}
+
+function closeImage() {
+  lightBox.style.display = "none";
+}
+
+thumbnails.forEach(image => {
+  image.addEventListener("click", () => popImage(image));
+});
+
+closeBtn.addEventListener("click", closeImage);
+
+// Close when clicking the lightbox background (but not the image)
+lightBox.addEventListener("click", (e) => {
+  if (e.target === lightBox) {
+    closeImage();
+  }
+});
